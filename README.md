2c039304959c9dcb889682b456830c10242fd953
Michael Hadley <michaelwesthadley@gmail.com>
2021-06-04 16:07:42 -0500

    Create gatsby site from template

---
+++ README.md
@@ -0,0 +1,12 @@
+# mikewesthad
+
+Personal Portfolio website: https://www.mikewesthad.com/
+
+## Usage
+
+Make sure you have node & npm installed, and then run:
+
+```
+npm install
+npm start
+```
---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>mikewesthad</title>
+      <h1>mikewesthad</h1>
+      <p>Hi, I’m Mike Hadley.</p>
+      <p>
+        I’m a developer, educator and artist who translates creative ideas into code and teaches
+        others how to do the same. My mission is to make the world of technology more accessible –
+        both in terms of who has access to learning materials as well as through introducing people
+        to the creative side of technology.
+      </p>
+      <p>Have a creative vision that you want to bring to life? Get in touch and let’s chat.</p>
+    </main>
+  );
+};
+
+export default IndexPage;
