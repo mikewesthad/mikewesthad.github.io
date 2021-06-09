ebc4ed961d932f4b7772db261ba35e8497944aaf
Michael Hadley <michaelwesthadley@gmail.com>
2021-06-09 16:05:49 -0500

    Add MDX project setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,14 @@ module.exports = {
         icon: "src/images/icon.png",
       },
     },
-    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/components/layout.tsx"),
+        },
+      },
+    },
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProjectListing() {
+  return <div>Project Listing</div>;
+}
+
+export default ProjectListing;
