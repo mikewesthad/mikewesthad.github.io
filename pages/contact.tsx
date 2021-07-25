f1db24df6275a79bb74ae5922d04bc2cc2ba08ee
Michael Hadley <michaelwesthadley@gmail.com>
2021-07-25 14:46:15 -0500

    Add placeholder contact page with formspree

---
+++ pages/contact.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+import { useForm, ValidationError } from "@formspree/react";
+import PageTitle from "../components/page-title";
+
+type FormProps = {
+  state: any;
+  handleSubmit: any;
+};
+
+const Form = ({ state, handleSubmit }: FormProps) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input required type="text" name="name" id="name" />
+        <ValidationError prefix="Name" field="name" errors={state.errors} />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input required type="email" name="email" id="email" />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+      </div>
+      <div>
+        <label htmlFor="message">Message</label>
+        <textarea required rows={4} name="message" id="message" />
+        <ValidationError prefix="Message" field="message" errors={state.errors} />
+      </div>
+      <button type="submit" disabled={state.submitting}>
+        {state.submitting ? "Submitting..." : "Submit →"}
+      </button>
+    </form>
+  );
+};
+
+const ContactPage = () => {
+  const [state, handleSubmit] = useForm("mqkwbzyb");
+
+  return (
+    <main>
+      <PageTitle>Contact</PageTitle>
+      <h1>Get in Touch</h1>
+      <p>
+        Looking to run a workshop on creative coding? Have a creative vision you want to bring to
+        life? Or, maybe you just want to say hello? Drop me a line.
+      </p>
+      {state.succeeded ? (
+        <p>Message sent! Thanks for reaching out.</p>
+      ) : (
+        <Form state={state} handleSubmit={handleSubmit} />
+      )}
+    </main>
+  );
+};
+
+export default ContactPage;
