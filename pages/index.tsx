1fece448a882775c52a9a492db1c03a58e63b66b
Michael Hadley <michaelwesthadley@gmail.com>
2021-07-25 15:04:55 -0500

    Update home links to point to other pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,14 +11,16 @@ const IndexPage = () => {
       <Image src={profileImage} alt="Mike Hadley"></Image>
       <p>Hi, I’m Mike Hadley.</p>
       <p>
-        I’m a <Link href="/">developer</Link>, <Link href="/">educator</Link> and{" "}
-        <Link href="/">artist</Link> who translates creative ideas into code and teaches others how
-        to do the same. My mission is to make the world of technology more accessible – both in
-        terms of who has access to learning materials as well as through introducing people to the
-        creative side of technology.
+        I’m a <Link href="/portfolio/dev">developer</Link>,{" "}
+        <Link href="/portfolio/teaching">educator</Link> and{" "}
+        <Link href="/portfolio/art">artist</Link> who translates creative ideas into code and
+        teaches others how to do the same. My mission is to make the world of technology more
+        accessible – both in terms of who has access to learning materials as well as through
+        introducing people to the creative side of technology.
       </p>
       <p>
-        Have a creative vision that you want to bring to life? Get in touch and let’s <a>chat</a>.
+        Have a creative vision that you want to bring to life? Get in touch and let’s{" "}
+        <Link href="/contact">chat</Link>.
       </p>
     </main>
   );
