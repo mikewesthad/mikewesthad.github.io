11bd5e4c238d22cb638492266ce2a5a02c0e49ed
Michael Hadley <michaelwesthadley@gmail.com>
2021-07-25 14:50:59 -0500

    Add placeholder work page

---
+++ pages/work.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import Link from "../components/link";
+import PageTitle from "../components/page-title";
+
+const WorkPage = () => {
+  return (
+    <main>
+      <PageTitle>Work</PageTitle>
+      <h1>Work</h1>
+      <p>TODO! This will list the main categories of work.</p>
+      <ul>
+        <li>Development</li>
+        <li>Open Source Tools</li>
+        <li>Art</li>
+      </ul>
+    </main>
+  );
+};
+
+export default WorkPage;
