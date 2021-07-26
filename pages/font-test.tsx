cf467fbaa42c879ed2bceae011aad94d601af07d
Michael Hadley <michaelwesthadley@gmail.com>
2021-07-26 17:52:15 -0500

    Fix h5 on font test page

--- pages/font-test.tsx
+++ pages/font-test.tsx
@@ -9,7 +9,7 @@ const FontTestPage = () => {
       <h2>Heading 2</h2>
       <h3>Heading 3</h3>
       <h4>Heading 4</h4>
-      <h4>Heading 5</h4>
+      <h5>Heading 5</h5>
       <p>
         Praesent a ante non ante malesuada ullamcorper vel at odio. Aenean pharetra, massa ac
         ultrices pretium, magna felis rutrum urna, sit amet gravida augue odio ac massa.
