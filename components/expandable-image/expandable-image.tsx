cb53dc6ca244fee684613e8ecfcf44c53466551b
Michael Hadley <michaelwesthadley@gmail.com>
2021-07-28 09:51:15 -0500

    Add ExpandableImage for images that need to be zoomed to full res

---
+++ components/expandable-image/expandable-image.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import Image from "next/image";
+import css from "./expandable-image.module.scss";
+import { IoMdClose } from "react-icons/io";
+
+interface ExpandedImageProps {
+  children: React.ReactNode;
+  onClose: () => void;
+}
+
+function ExpandedImage({ children, onClose }: ExpandedImageProps) {
+  return (
+    <div className={css.expandedImageWrapper}>
+      <button onClick={onClose} className={css.closeButton}>
+        <IoMdClose />
+      </button>
+      {children}
+    </div>
+  );
+}
+
+interface ExpandableImageProps {
+  src: StaticImageData;
+}
+
+function ExpandableImage({ src }: ExpandableImageProps) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const onClick = () => setIsExpanded(!isExpanded);
+  const close = () => setIsExpanded(false);
+
+  return (
+    <div>
+      {isExpanded && (
+        <ExpandedImage onClose={close}>
+          <Image src={src} onClick={onClick} className={css.expandableImage} />
+        </ExpandedImage>
+      )}
+      <Image src={src} onClick={onClick} className={css.expandableImage} />
+    </div>
+  );
+}
+
+export default ExpandableImage;
