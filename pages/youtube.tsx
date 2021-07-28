71af12e29dbcf4ca9e59f6a28d48e8db719b1251
Michael Hadley <michaelwesthadley@gmail.com>
2021-07-28 09:56:11 -0500

    Use container on YT page

--- pages/youtube.tsx
+++ pages/youtube.tsx
@@ -1,36 +1,41 @@
 import * as React from "react";
+import Container from "../components/container/container";
 import PageTitle from "../components/page-title";
 import YouTubeEmbed from "../components/youtube-embed";
 
 const YouTubePage = () => {
   return (
-    <main>
+    <Container>
       <PageTitle>YouTube</PageTitle>
-      <h1>YouTube Playlists</h1>
-      <h2>Introduction to Programming in C#</h2>
-      <YouTubeEmbed
-        width="560"
-        height="315"
-        videoId="9TqENGhldWs"
-        playlistId="PL-LDQE9x9hLwldZPPGwqXixr-_DfINfxk"
-      />
+      <main>
+        <h1>YouTube Playlists</h1>
+        <h2>Introduction to Programming in C#</h2>
+        <YouTubeEmbed
+          width="560"
+          height="315"
+          videoId="9TqENGhldWs"
+          playlistId="PL-LDQE9x9hLwldZPPGwqXixr-_DfINfxk"
+        />
 
-      <h2>Web Application Programming - Advanced Web Development (HTML/CSS/JS, React, Firebase)</h2>
-      <YouTubeEmbed
-        width="560"
-        height="315"
-        videoId="OnYyB0_lzcg"
-        playlistId="PL-LDQE9x9hLwUAESe_YJxhXU5ZjLgtq4S"
-      />
+        <h2>
+          Web Application Programming - Advanced Web Development (HTML/CSS/JS, React, Firebase)
+        </h2>
+        <YouTubeEmbed
+          width="560"
+          height="315"
+          videoId="OnYyB0_lzcg"
+          playlistId="PL-LDQE9x9hLwUAESe_YJxhXU5ZjLgtq4S"
+        />
 
-      <h2>Code Sprints - Beginner Web App Development (HTML/CSS/JS, React, Firebase)</h2>
-      <YouTubeEmbed
-        width="560"
-        height="315"
-        videoId="CLy35rh8DpA"
-        playlistId="PL-LDQE9x9hLyowrXuGkk53uVPbljucpdK"
-      />
-    </main>
+        <h2>Code Sprints - Beginner Web App Development (HTML/CSS/JS, React, Firebase)</h2>
+        <YouTubeEmbed
+          width="560"
+          height="315"
+          videoId="CLy35rh8DpA"
+          playlistId="PL-LDQE9x9hLyowrXuGkk53uVPbljucpdK"
+        />
+      </main>
+    </Container>
   );
 };
 
