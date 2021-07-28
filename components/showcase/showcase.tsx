54858c875b5923139e5e4a204eee3e96728dce35
Michael Hadley <michaelwesthadley@gmail.com>
2021-07-28 09:55:58 -0500

    Test out showcase on dev page

---
+++ components/showcase/showcase.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Image from "next/image";
+import css from "./showcase.module.scss";
+import Link from "../link";
+
+interface ShowcaseItemProps {
+  title: string;
+  subtitle: string;
+  image: StaticImageData;
+}
+
+function ShowcaseItem({ image, title, subtitle }: ShowcaseItemProps) {
+  return (
+    <li>
+      <Link className="unstyled-link" href="#">
+        <figure className={css.showcaseItem}>
+          <Image src={image} width={250} height={(250 * 9) / 16} objectFit="cover" />
+          <figcaption>
+            <div className={css.showcaseItemTitle}>{title}</div>
+            <div className={css.showcaseItemSubtitle}>{subtitle}</div>
+          </figcaption>
+        </figure>
+      </Link>
+    </li>
+  );
+}
+
+interface ShowcaseProps {
+  children: React.ReactNode;
+}
+
+function Showcase({ children }: ShowcaseProps) {
+  return <ul className={css.showcase}>{children}</ul>;
+}
+
+export default Showcase;
+export { ShowcaseItem };
+export type { ShowcaseItemProps, ShowcaseProps };
