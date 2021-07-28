4d92270b0368ed4b557c84ed697600c5aa7dff62
Michael Hadley <michaelwesthadley@gmail.com>
2021-07-28 09:51:57 -0500

    Split up nav in its own folder and use social links

---
+++ components/nav/nav-link.tsx
@@ -0,0 +1,24 @@
+import { useRouter } from "next/router";
+import Link from "../link/link";
+import css from "./nav-link.module.scss";
+
+interface NavLinkProps {
+  href: string;
+  children: React.ReactNode;
+  exact?: boolean;
+}
+
+function NavLink({ href, children, exact = false }: NavLinkProps) {
+  const { pathname } = useRouter();
+  const isActive = exact ? pathname === href : pathname.startsWith(href);
+  let className = `unstyled-link ${css.navLink}`;
+  if (isActive) className += ` ${css.navLinkActive}`;
+  return (
+    <Link className={className} href={href}>
+      {children}
+    </Link>
+  );
+}
+
+export default NavLink;
+export type { NavLinkProps };
---
+++ components/nav/nav.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { useRouter } from "next/router";
+import Link from "../link/link";
+import css from "./nav.module.scss";
+import Container from "../container/container";
+import { GitHubIconLink, LinkedInIconLink, YouTubeIconLink } from "../social-links/social-links";
+import NavLink from "./nav-link";
+
+function Nav() {
+  return (
+    <>
+      <Container>
+        <nav className={css.nav}>
+          <Link href="/" className={`unstyled-link ${css.navLogo}`}>
+            mikewesthad
+          </Link>
+          <ul className={css.navList}>
+            <li className={css.dropdownTrigger}>
+              <NavLink href="/" exact={true}>
+                Portfolio
+              </NavLink>
+              <ul className={css.dropdownList}>
+                <li>
+                  <NavLink href="/dev">Dev</NavLink>
+                </li>
+                <li>
+                  <NavLink href="/edu">Edu</NavLink>
+                </li>
+                <li>
+                  <NavLink href="/art">Art</NavLink>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <NavLink href="/blog">Blog</NavLink>
+            </li>
+            <li>
+              <NavLink href="/contact">Contact</NavLink>
+            </li>
+            <li>
+              <ul className={css.socialLinks}>
+                <li>
+                  <GitHubIconLink />
+                </li>
+                <li>
+                  <LinkedInIconLink />
+                </li>
+                <li>
+                  <YouTubeIconLink />
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+      </Container>
+      <hr />
+    </>
+  );
+}
+
+export default Nav;
