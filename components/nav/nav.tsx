9b2701d43eb62098879bf3d99e9b4703840f200b
Michael Hadley <michaelwesthadley@gmail.com>
2021-07-28 18:05:21 -0500

    Refactor nav to use Container as nav element

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -9,49 +9,47 @@ import NavLink from "./nav-link";
 function Nav() {
   return (
     <>
-      <Container>
-        <nav className={css.nav}>
-          <Link href="/" className={`unstyled-link ${css.navLogo}`}>
-            mikewesthad
-          </Link>
-          <ul className={css.navList}>
-            <li className={css.dropdownTrigger}>
-              <NavLink href="/" exact={true}>
-                Portfolio
-              </NavLink>
-              <ul className={css.dropdownList}>
-                <li>
-                  <NavLink href="/dev">Dev</NavLink>
-                </li>
-                <li>
-                  <NavLink href="/edu">Edu</NavLink>
-                </li>
-                <li>
-                  <NavLink href="/art">Art</NavLink>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <NavLink href="/blog">Blog</NavLink>
-            </li>
-            <li>
-              <NavLink href="/contact">Contact</NavLink>
-            </li>
-            <li>
-              <ul className={css.socialLinks}>
-                <li>
-                  <GitHubIconLink />
-                </li>
-                <li>
-                  <LinkedInIconLink />
-                </li>
-                <li>
-                  <YouTubeIconLink />
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </nav>
+      <Container tagName="nav" className={css.nav}>
+        <Link href="/" className={`unstyled-link ${css.navLogo}`}>
+          mikewesthad
+        </Link>
+        <ul className={css.navList}>
+          <li className={css.dropdownTrigger}>
+            <NavLink href="/" exact={true}>
+              Portfolio
+            </NavLink>
+            <ul className={css.dropdownList}>
+              <li>
+                <NavLink href="/dev">Dev</NavLink>
+              </li>
+              <li>
+                <NavLink href="/edu">Edu</NavLink>
+              </li>
+              <li>
+                <NavLink href="/art">Art</NavLink>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <NavLink href="/blog">Blog</NavLink>
+          </li>
+          <li>
+            <NavLink href="/contact">Contact</NavLink>
+          </li>
+          <li>
+            <ul className={css.socialLinks}>
+              <li>
+                <GitHubIconLink />
+              </li>
+              <li>
+                <LinkedInIconLink />
+              </li>
+              <li>
+                <YouTubeIconLink />
+              </li>
+            </ul>
+          </li>
+        </ul>
       </Container>
       <hr />
     </>
