793b63e8e923270278b5574f513cb062a01eb4ca
Michael Hadley <michaelwesthadley@gmail.com>
2021-07-29 18:21:25 -0500

    Add placeholder redirects for old site to new site

---
+++ next.config.js
@@ -0,0 +1,45 @@
+// In moving to Next, things have been restructured/omitted, so make sure to set
+// up some redirects to make sure all relevant resume links still work.
+// TODO: fill these in with the final routes.
+const oldSiteRedirects = [
+  { from: "/work", to: "/" },
+  { from: "/projects/encoded-objects", to: "/" },
+  { from: "/projects/togetheroo", to: "/" },
+  { from: "/projects/edovo", to: "/" },
+  { from: "/projects/sporadic-labs", to: "/" },
+  { from: "/projects/ofbook", to: "/" },
+  { from: "/projects/msp-game-design", to: "/portfolio/edu" },
+  { from: "/projects/cps-convergence", to: "/portfolio/edu" },
+  { from: "/projects/uptake-game-design", to: "/portfolio/edu" },
+  { from: "/projects/laf-creative-coding", to: "/portfolio/edu" },
+  { from: "/projects/codeabode-teaching", to: "/portfolio/edu" },
+  { from: "/projects/saic-web-art", to: "/portfolio/edu" },
+  { from: "/projects/uic-interactive-3d", to: "/portfolio/edu" },
+  { from: "/projects/uic-gameplay", to: "/portfolio/edu" },
+  { from: "/projects/berkeley-retinal-modeling", to: "/portfolio/science" },
+  { from: "/projects/laf-network-analysis", to: "/portfolio/science" },
+  { from: "/projects/laf-multisensory", to: "/portfolio/science" },
+  { from: "/projects/view-history", to: "/portfolio/art" },
+  { from: "/projects/promise-foods", to: "/portfolio/art" },
+  { from: "/projects/gratuitous-gram", to: "/portfolio/art" },
+  { from: "/projects/blink", to: "/portfolio/art" },
+  { from: "/projects/public-insecurities", to: "/portfolio/art" },
+  { from: "/projects/j4kdart", to: "/404" },
+  { from: "/projects/youtube-spaces", to: "/" },
+  { from: "/projects/emojify-tracking", to: "/" },
+  { from: "/projects/html-form-renderer", to: "/" },
+  { from: "/projects/render-sketch-game", to: "/" },
+  { from: "/projects/video-collapse", to: "/" },
+  { from: "/projects/natural-symmetries", to: "/" },
+  { from: "/projects/disconnect", to: "/" },
+];
+
+module.exports = {
+  async redirects() {
+    return oldSiteRedirects.map(({ from, to }) => ({
+      source: `${from}(.html)?`,
+      destination: to,
+      permanent: true,
+    }));
+  },
+};
