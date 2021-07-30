2a5922b45c0e0ede3a0d39da73bb289c7e96b75a
Michael Hadley <michaelwesthadley@gmail.com>
2021-07-30 17:21:00 -0500

    Use section on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,63 +1,59 @@
 import Image from "next/image";
 import Link from "components/link";
-import profileImage from "images/mike-hadley-profile.jpg";
 import PageTitle from "components/page-title";
-import Container from "components/container";
-
-const Grid = ({ children }: { children: React.ReactNode }) => (
-  <div>
-    {children}
-    <style jsx>{`
-      div {
-        display: flex;
-        justify-content: space-between;
-      }
-    `}</style>
-  </div>
-);
+import Section from "components/container/section";
+import profileImage from "images/mike-hadley-profile.jpg";
 
 const IndexPage = () => {
   return (
-    <Container tagName="main">
+    <main>
       <PageTitle>Home</PageTitle>
-      <Grid>
-        <div>
-          <h1>Hi 👋, I’m Mike Hadley</h1>
-          <h2>Mission Statement</h2>
-          <p>
-            I’m a developer, educator and artist who translates creative ideas into code and teaches
-            others how to do the same. My mission is to make the world of technology more accessible
-            – both in terms of who has access to learning materials as well as through introducing
-            people to the creative side of technology.
-          </p>
-        </div>
-        <Image src={profileImage} alt="Mike Hadley" objectFit="scale-down" />
-        <style jsx global>{`
-          div {
-          }
-          img {
+      <Section>
+        <figure>
+          <Image src={profileImage} alt="Mike Hadley" layout="fill" objectFit="cover" />
+        </figure>
+        <style jsx>{`
+          figure {
+            position: relative;
+            width: 10rem;
+            height: 10rem;
             border-radius: 50%;
+            overflow: hidden;
+            margin: 0 auto;
           }
         `}</style>
-      </Grid>
-      <p>
-        Have a creative vision that you want to bring to life? Get in touch and{" "}
-        <Link href="/contact">let's chat</Link>.
-      </p>
-      <h2>Portfolio</h2>
-      <p>Want to see examples of my work? It's broken down into the following categories:</p>
-      <ul>
-        <li>
-          <Link href="/portfolio/dev">Development</Link>
-        </li>
-        <li>
-          <Link href="/portfolio/edu">Teaching</Link>
-        </li>
-        <li>
-          <Link href="/portfolio/art">Art</Link>
-        </li>
-      </ul>
-    </Container>
+      </Section>
+
+      <Section>
+        <h1>Hi 👋, I’m Mike Hadley</h1>
+        <h2>Mission Statement</h2>
+        <p>
+          I’m a developer, educator and artist who translates creative ideas into code and teaches
+          others how to do the same. My mission is to make the world of technology more accessible –
+          both in terms of who has access to learning materials as well as through introducing
+          people to the creative side of technology.
+        </p>
+        <p>
+          Have a creative vision that you want to bring to life? Get in touch and{" "}
+          <Link href="/contact">let's chat</Link>.
+        </p>
+      </Section>
+      <Section>
+        <h2>Portfolio</h2>
+        <p>Want to see examples of my work? It's broken down into the following categories:</p>
+        <ul>
+          <li>
+            <Link href="/portfolio/dev">Development</Link>
+          </li>
+          <li>
+            <Link href="/portfolio/edu">Teaching</Link>
+          </li>
+          <li>
+            <Link href="/portfolio/art">Art</Link>
+          </li>
+        </ul>
+      </Section>
+    </main>
   );
 };
 
