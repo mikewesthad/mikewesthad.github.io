bed9194f5a33b791a0d4ecdb5cf9325051fe800f
Michael Hadley <michaelwesthadley@gmail.com>
2021-07-29 22:26:26 -0500

    Blur NavLink on select to close dropdowns

--- components/nav/nav-link.tsx
+++ components/nav/nav-link.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import Link from "components/link";
 import css from "./nav-link.module.scss";
+import React from "react";
 
 interface NavLinkProps {
   href: string;
@@ -14,8 +15,17 @@ function NavLink({ href, children, tiltLeft = true, exact = false }: NavLinkProp
   const isActive = exact ? pathname === href : pathname.startsWith(href);
   let className = `unstyled-link ${tiltLeft ? css.tiltLeft : css.tiltRight} ${css.navLink}`;
   if (isActive) className += ` ${css.navLinkActive}`;
+
+  // Placeholder: blur nav links when the user clicks (or keyboard "clicks") on them
+  // to close the dropdown and remove the focus. Verify this is accessible!
+  const onClick: React.MouseEventHandler<HTMLAnchorElement> = (e) => {
+    if (e.target === document.activeElement) {
+      (document.activeElement as HTMLAnchorElement).blur();
+    }
+  };
+
   return (
-    <Link className={className} href={href}>
+    <Link className={className} href={href} anchorProps={{ onClick }}>
       {children}
     </Link>
   );
