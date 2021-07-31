da4608b7e932a41a8cffc6ac6a4c40cf761f745f
Michael Hadley <michaelwesthadley@gmail.com>
2021-07-30 20:00:50 -0500

    Add comment to clarify "fix"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,9 @@ import Meta from "components/meta";
 import useFoucFix from "../utils/use-fouc-fix";
 
 export default function MyApp({ Component, pageProps, router }: AppProps) {
-  // Fix for CSS module styling being unloaded in transitions.
+  // "Fix" for CSS module styling being unloaded in transitions. Hopefully, this
+  // can be removed when the bug in Next.js is fixed. Otherwise, switching to
+  // styled JSX would fix it...
   useFoucFix();
 
   const pageKey = router.route;
