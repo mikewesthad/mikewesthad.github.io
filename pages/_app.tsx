3722e49f98767f7faed8a7e52e1e8f456cd9d87d
Michael Hadley <michaelwesthadley@gmail.com>
2021-07-30 19:39:34 -0500

    "Fix" for CSS modules unloading during transitions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,12 @@ import "../global-styles/index.scss";
 import "@fontsource/raleway";
 import SiteLayout from "components/site-layout";
 import Meta from "components/meta";
+import useFoucFix from "../utils/use-fouc-fix";
 
 export default function MyApp({ Component, pageProps, router }: AppProps) {
+  // Fix for CSS module styling being unloaded in transitions.
+  useFoucFix();
+
   const pageKey = router.route;
   return (
     <SiteLayout pageKey={pageKey}>
