3570346fddeccbd821d5e54429407979f2909366
Michael Hadley <michaelwesthadley@gmail.com>
2021-08-01 12:33:41 -0500

    Edit fin lit page

--- pages/portfolio/financial-literacy/index.tsx
+++ pages/portfolio/financial-literacy/index.tsx
@@ -2,8 +2,14 @@ import Image from "next/image";
 import ProjectPage from "components/project-page";
 import Section from "components/container/section";
 import ImageGallery from "components/image-gallery";
-import image1 from "./images/budget-1.png";
-import image2 from "./images/credit-power-1.png";
+import image1 from "./images/loan-1.png";
+import image2 from "./images/loan-2.png";
+import image3 from "./images/loan-3.png";
+import image4 from "./images/credit-1.png";
+import image5 from "./images/credit-3.png";
+import image6 from "./images/credit-4.png";
+import image7 from "./images/budget-1.png";
+import image8 from "./images/budget-2.png";
 
 function Phaser() {
   return (
@@ -14,11 +20,13 @@ function Phaser() {
       <Section>
         <h2>Overview</h2>
         <p>
-          As part of my role as Learning Product Developer at Columbia College Chicago, I created
-          digital games and tools for partner organizations. I designed and developed a series of
-          serious games (JS, React) for clients around topics like financial literacy for the{" "}
-          <a href="https://www.econcouncil.org/">Economic Awareness Council</a> and{" "}
-          <a href="https://www.home.lrng.org/">LRNG</a>, which reached thousands of Chicago youth.
+          As part of my role as Learning Product Developer at{" "}
+          <a href="https://www.colum.edu/">Columbia College Chicago</a>, I created a series of
+          digital games and tools for partner organizations. One of those partners was the{" "}
+          <a href="https://www.econcouncil.org/">Economic Awareness Council</a>, a non-profit whose
+          mission is equip youth with the skills needed to make economic and financial decisions. I
+          designed and developed a series of serious games (JS, React) for them around financial
+          literacy, which have reached thousands of Chicago youth so far.
         </p>
         <p>
           These games and tools were launched as part of "learning playlists" on financial literacy,
@@ -26,7 +34,7 @@ function Phaser() {
           concepts like budgeting and credit.
         </p>
       </Section>
-      <ImageGallery images={[image1, image2]} />
+      <ImageGallery images={[image1, image2, image3, image4, image5, image6, image7, image8]} />
     </ProjectPage>
   );
 }
