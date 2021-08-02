e5d1554a4e288e2df8c3a4db6d4f1d8aa33c3726
Michael Hadley <michaelwesthadley@gmail.com>
2021-08-01 20:04:39 -0500

    Refactor Link to handle external links as well as internal links

--- components/link.tsx
+++ components/link.tsx
@@ -1,14 +1,33 @@
 import NextLink, { LinkProps as NextLinkProps } from "next/link";
+import { IoOpenOutline } from "react-icons/io5";
+import css from "./link.module.scss";
 
 interface LinkProps extends NextLinkProps {
   children: React.ReactNode;
   className?: string;
   anchorProps?: React.HTMLProps<HTMLAnchorElement>;
+  showExternalIcon?: boolean;
 }
 
-function Link({ anchorProps, className, children, ...otherProps }: LinkProps) {
+function Link({
+  anchorProps,
+  className,
+  href,
+  showExternalIcon = true,
+  children,
+  ...otherProps
+}: LinkProps) {
+  if (typeof href === "string" && href.startsWith("http")) {
+    return (
+      <a href={href} className={className} {...anchorProps}>
+        {children}
+        {showExternalIcon && <IoOpenOutline className={css.externalLinkSvg} />}
+      </a>
+    );
+  }
+
   return (
-    <NextLink scroll={false} {...otherProps}>
+    <NextLink scroll={false} href={href} {...otherProps}>
       <a className={className} {...anchorProps}>
         {children}
       </a>
